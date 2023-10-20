1c2a6d0415ea312098a3736e0d173bc1ae537c88
donperro05 <santianqr@gmail.com>
2023-10-19 23:47:53 -0500

    carousel-update

--- components/PhotosSection.tsx
+++ components/PhotosSection.tsx
@@ -2,33 +2,32 @@ import type { NextPage } from "next";
 import Carousel from "@/components/Carousel";
 import Image from "next/image";
 
-import image1 from "@/public/musculoso.webp"
-import image2 from "@/public/niche.webp"
+import image1 from "@/public/musculoso.webp";
+import image2 from "@/public/niche.webp";
 
 const PhotosSection: NextPage = () => {
-  const images = [
-    image1,
-    image2,
-  ];
+  const images = [image1, image2];
   return (
-    <div className="sm:w-3/4 mx-auto my-2">
-      <Carousel loop>
+    <section className="container max-w-screen-lg mx-auto px-6 relative">
+      <Carousel loop >
         {images.map((src, i) => {
           return (
-            // 👇 style each individual slide.
-            // relative - needed since we use the fill prop from next/image component
-            // h-64 - arbitrary height
-            // flex[0_0_100%]
-            //   - shorthand for flex-grow:0; flex-shrink:0; flex-basis:100%
-            //   - we want this slide to not be able to grow or shrink and take up 100% width of the viewport.
-            <div className="relative h-64 flex-[0_0_100%]" key={i}>
-              {/* use object-cover + fill since we don't know the height and width of the parent */}
-              <Image src={src} fill className="object-cover" alt="alt" />
+            <div className="relative w-full flex-shrink-0" key={i}>
+              <Image
+                src={src}
+                className="object-cover"
+                alt="alt"
+                sizes="100vw"
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent"></div>
             </div>
           );
         })}
       </Carousel>
-    </div>
+    </section>
   );
 };
 
