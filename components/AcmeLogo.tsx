d8370b547faaa772eaf3b62f512058494d11a92b
donperro05 <santianqr@gmail.com>
2023-10-26 16:48:12 -0500

    migration

--- components/AcmeLogo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Image from "next/image";
-import logo from "@/public/logo.webp";
-
-export const AcmeLogo = () => (
-  <Image alt="logo" src={logo} width={50} height={50} />
-);
