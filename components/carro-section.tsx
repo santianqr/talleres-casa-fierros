b6649c5a0f3cae9200559922937f9276b0df39c3
donperro05 <santianqr@gmail.com>
2023-11-26 19:46:08 -0500

    carro_section_full

--- components/carro-section.tsx
+++ components/carro-section.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { FaTools } from "react-icons/fa";
 
 export default function CarroSection() {
   return (
     <section className="w-full py-4 px-6 flex justify-center">
-      <div className="max-w-[1024px] w-[100%] flex flex-row justify-between items-center">
-        <div className="relative w-[35%] h-[30vh]">
+      <div className="max-w-[1024px] w-[100%] flex flex-col sm:flex-row justify-between items-center bg-yellow-500/[0.6] gap-y-4 sm:gap-y-0">
+        <div className="hidden sm:block sm:relative sm:w-[35%] sm:h-[30vh]">
           <Image
             alt="..."
             src={"/medio_carro.png"}
@@ -15,19 +16,40 @@ export default function CarroSection() {
             className=" "
           />
         </div>
-        <div className="flex flex-row w-[60%] justify-between px-6">
-            <div className="flex flex-col gap-4">
-                <p className="font-bold text-2xl">15</p>
-                <p className="text-sm">AÑOS DE<br/>EXPERIENCIA</p>
+        <div className="flex flex-col sm:flex sm:flex-row sm:w-[60%] sm:justify-between gap-y-6 sm:px-6">
+          <div className="flex flex-col gap-2 items-center">
+            <p className="font-bold text-2xl">15</p>
+            <div className="flex flex-row gap-2 items-center">
+              <FaTools />
+              <p className="text-sm">
+                AÑOS DE
+                <br />
+                EXPERIENCIA
+              </p>
             </div>
-            <div className="flex flex-col gap-4">
-                <p className="font-bold text-2xl">1020</p>
-                <p className="text-sm">CLIENTES<br/>ATENDIDOS</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            <p className="font-bold text-2xl">1020</p>
+            <div className="flex flex-row gap-2 items-center">
+              <FaTools />
+              <p className="text-sm">
+                CLIENTES
+                <br />
+                ATENDIDOS
+              </p>
             </div>
-            <div className="flex flex-col gap-4">
-                <p className="font-bold text-2xl">20</p>
-                <p className="text-sm">MECÁNICOS<br/>PROFESIONALES</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            <p className="font-bold text-2xl">20</p>
+            <div className="flex flex-row gap-2 items-center">
+              <FaTools />
+              <p className="text-sm">
+                MECÁNICOS
+                <br />
+                PROFESIONALES
+              </p>
             </div>
+          </div>
         </div>
       </div>
     </section>
