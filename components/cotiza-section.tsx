0a9b9a22ad200125b8d3ae57ab633db6f09fecb5
donperro05 <santianqr@gmail.com>
2023-11-28 15:10:40 -0500

    cotiza-60%

--- components/cotiza-section.tsx
+++ components/cotiza-section.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { GoogleMapsEmbed } from "@next/third-parties/google";
+import { Button } from "@nextui-org/react";
 
 export default function CotizaSection() {
   return (
@@ -16,8 +17,8 @@ export default function CotizaSection() {
             </div>
           </div>
         </div>
-        <div>
-          <div className="w-64">
+        <div className="flex flex-row">
+          <div className="w-[60%]">
             <GoogleMapsEmbed
               apiKey="AIzaSyAaegn0N4t6OBIQBwhVVlZ2GepBvD9mSkg"
               height={400}
@@ -29,8 +30,61 @@ export default function CotizaSection() {
               style="border-radius:1rem;"
             />
           </div>
-          <div>
-            
+          <div className="flex flex-col gap-y-6 w-[40%] justify-between">
+            <div className="flex flex-row gap-x-6 justify-between">
+              <div className="flex flex-col justify-between">
+                <input
+                  type="text"
+                  placeholder="Nombre"
+                  className="bg-background border-b-2 border-yellow-500/[0.6] focus:border-yellow-500 outline-none"
+                />
+                <input
+                  type="text"
+                  placeholder="Celular"
+                  className="bg-background border-b-2 border-yellow-500/[0.6] focus:border-yellow-500 outline-none"
+                />
+                <input
+                  type="text"
+                  placeholder="no se que dice"
+                  className="bg-background border-b-2 border-yellow-500/[0.6] focus:border-yellow-500 outline-none"
+                />
+              </div>
+              <div>
+                <p>SERVICIO</p>
+                <label className="flex items-center space-x-2">
+                  <input type="checkbox" name="opciones" value="opcion1" />
+                  <span>Mecánica básica</span>
+                </label>
+                <label className="flex items-center space-x-2">
+                  <input type="checkbox" name="opciones" value="opcion2" />
+                  <span>Mecánica especializada</span>
+                </label>
+                <label className="flex items-center space-x-2">
+                  <input type="checkbox" name="opciones" value="opcion3" />
+                  <span>Electricidad automotriz</span>
+                </label>
+                <label className="flex items-center space-x-2">
+                  <input type="checkbox" name="opciones" value="opcion4" />
+                  <span>Latoneria y pintura</span>
+                </label>
+                <label className="flex items-center space-x-2">
+                  <input type="checkbox" name="opciones" value="opcion5" />
+                  <span>Servicios especiales</span>
+                </label>
+              </div>
+            </div>
+            <div className="flex flex-col gap-y-4">
+              <input
+                type="text"
+                placeholder="tampoco se que dice"
+                className="w-[100%] bg-background border-b-2 border-yellow-500/[0.6] focus:border-yellow-500 outline-none"
+              />
+              <label className="flex items-center space-x-2">
+                <input type="checkbox" />
+                <span>Acepto los términos y condiciones</span>
+              </label>
+            </div>
+            <Button color="warning">Cotizar</Button>
           </div>
         </div>
       </div>
