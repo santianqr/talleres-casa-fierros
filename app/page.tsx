f39f33c764c21a66d58c058b89445d68535b8c76
donperro05 <santianqr@gmail.com>
2023-12-03 16:40:51 -0500

    footer_50%

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
       <CarouselBrand />
       <AboutSection />
       <CarroSection />
-      <PrefSection />*/}
+      <PrefSection />
       <AgendaSection />
-      <CotizaSection />
-      {/*<Footer />*/}
+      <CotizaSection />*/}
+      <Footer />
     </main>
   );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -10,44 +10,42 @@ export default function Footer() {
   return (
     <footer className="w-full py-4 px-6 flex justify-center">
       <div className="max-w-[1024px] w-[100%] border-t-1 border-t-yellow-500 h-[20vh] flex flex-row">
-        <div className="flex flex-row w-[50%]">
+        <div className="flex flex-row w-[50%] justify-between px-10 items-center">
           <div className="relative w-36 h-28">
             <Image src={"/logo_color.png"} alt="" fill />
           </div>
-          <div>
-            <div className="flex flex-row">
+          <div className="flex flex-col gap-y-3">
+            <div className="flex flex-row items-center">
               <FaLocationDot />
-              <p>Cl. 161a #16a-58</p>
+              <p className="px-2">Cl. 161a #16a-58</p>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row items-center">
               <MdOutlineSmartphone />
-              <p>
+              <p className="px-2">
                 324 582 7230
                 <br />
                 350780050
               </p>
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row items-center">
               <MdEmail />
-              <p>atencion@tallerescasafierros.co</p>
+              <p className="px-2">atencion@tallerescasafierros.co</p>
             </div>
           </div>
         </div>
-        <div className="flex flex-row w-[50%] justify-between">
-          <div><div>
+        <div className="flex flex-row w-[50%] justify-between px-10 items-center pt-8">
+          <div className="text-sm">
             <p>Inicio</p>
             <p>Servicios</p>
             <p>Blog</p>
             <p>Contacto</p>
             <p>Nosotros</p>
             <p>Trabaja con nosotros</p>
-          </div>
-          <div>
+            <br />
             <p>Horario de atención</p>
             <p>Luves - Viernes/7am - 5pm</p>
-            <p>Sabados /8am-3pm</p>s
-          </div></div>
-          
+            <p>Sabados /8am-3pm</p>
+          </div>
 
           <div>
             <div>
