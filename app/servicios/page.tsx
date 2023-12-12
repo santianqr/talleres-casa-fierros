18096df7cd79cee0de0267257b3bc179b328e405
donperro05 <santianqr@gmail.com>
2023-12-11 19:23:04 -0500

    update-servicios

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -5,18 +5,48 @@ import { Accordion, AccordionItem } from "@nextui-org/react";
 export default function Servicios() {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const size = 24;
+  const rows = new Array(6).fill(null);
+  const cols = new Array(size).fill(null);
 
   return (
-    <Accordion selectionMode="multiple">
-      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-        {defaultContent}
-      </AccordionItem>
-    </Accordion>
+    <main className="w-full py-4 px-6 flex justify-center">
+      <div className="max-w-[1024px] w-[100%]">
+        <p className="text-center">SERVICIOS</p>
+        <div className="flex flex-wrap justify-center h-[20vh] relative">
+          <div
+            className="grid w-full h-[100%] gap-0.5"
+            style={{ gridTemplateColumns: `repeat(${size}, 1fr)` }}
+          >
+            {rows.map((_, i) =>
+              cols.map((_, j) => (
+                <div
+                  key={`${i}-${j}`}
+                  className={`relative ${
+                    i % 2 === j % 2 ? "bg-yellow-500" : "bg-black"
+                  }`}
+                  style={{ paddingBottom: `${100 / size}%` }}
+                >
+                  <div className="absolute inset-0"></div>
+                </div>
+              ))
+            )}
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent"></div>
+          <div className="w-full bg-gray-500 flex">
+            ddsa
+            <br />
+            fsf
+            <br />
+            fds
+            <br />
+            fds
+            <br />
+            <div className="w-1/2 bg-gradient-to-r from-transparent via-black to-black" />
+            <div className="w-1/2 bg-gradient-to-l from-transparent via-black to-black" />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
