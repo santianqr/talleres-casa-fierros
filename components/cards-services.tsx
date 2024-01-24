db33a1e3921e64563e2a8ac0e586c9f8c5fe459b
donperro05 <santianqr@gmail.com>
2024-01-23 22:48:07 -0500

    Add CardsServices component with multiple cards

--- components/cards-services.tsx
+++ components/cards-services.tsx
@@ -1,5 +1,166 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@nextui-org/react";
+import React from "react";
+
 export default function CardsServices() {
   return (
-    <section>dsadas</section>
-  )
+    <section className="w-full py-4 px-6 flex justify-center">
+      <div className="max-w-[1024px] w-[100%] grid grid-cols-3 gap-4">
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">NextUI</p>
+              <p className="text-small text-default-500">nextui.org</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/nextui-org/nextui"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">NextUI</p>
+              <p className="text-small text-default-500">nextui.org</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/nextui-org/nextui"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">NextUI</p>
+              <p className="text-small text-default-500">nextui.org</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/nextui-org/nextui"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">NextUI</p>
+              <p className="text-small text-default-500">nextui.org</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/nextui-org/nextui"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">NextUI</p>
+              <p className="text-small text-default-500">nextui.org</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://github.com/nextui-org/nextui"
+            >
+              Visit source code on GitHub.
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
 }
